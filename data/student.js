222159d5dab76edae126ccb996ad20933e420d79
SatriaAN <satria182002@gmail.com>
2021-12-18 00:22:18 +0700

    tugas 11

---
+++ data/student.js
@@ -0,0 +1,8 @@
+// TODO 1: Buat data students
+// code here
+const students =
+  /* [(nama1 = "Sat"), (nama2 = "KI"), (nama3 = "Bon")]; */
+  ["Sat", "Rik", "Rom"];
+// TODO 2: export data students
+// code here
+module.exports = students;
